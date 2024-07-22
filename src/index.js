b06f2f7b42bff981bf4686d63a0c86f4e7afacf9
Nikhil Gurung <nikkhilgurung@gmail.com>
2024-07-22 13:40:28 +0530

    Third push

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is connected on port: ${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log("ERROR", err);
+})
 
 
 
