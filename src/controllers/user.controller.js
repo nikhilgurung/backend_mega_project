e186f8c8c120ca19c694010576402a760d3e8ca9
Nikhil Gurung <nikkhilgurung@gmail.com>
2024-07-23 13:22:01 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,7 +32,13 @@ const registerUser = asyncHandler(async (req, res) => {
   // checking if the avatar and coverimage is there or not
   const avatarLocalPath = req.files?.avatar[0]?.path;
 
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+    coverImageLocalPath  =req.files.coverImage[0].path
+  }
+
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required!!");
   }
