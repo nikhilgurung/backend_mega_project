df2d755d48ed7227bad71b4421f90af908b04163
Nikhil Gurung <nikkhilgurung@gmail.com>
2024-07-23 13:03:00 +0530

    5th commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,7 +23,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "all fields are required");
   }
   // checking if the user exists or not
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./db/db.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 
